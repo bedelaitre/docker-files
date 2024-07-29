1185e67cb7383291bdc8c213eec669270c75a570
bedelaitre <bertrand@delaitre.fr>
2024-07-29 07:40:51 +0200

    Delete ubuntu/src/home/Documents/installation directory

--- ubuntu/src/home/Documents/installation/cron_calibre_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Vérifier si le processus calibre est en cours d'exécution
-if ! ps aux | grep -v grep | grep -q "calibre-server"
-then
-    # Lancer calibre
-    calibre-server --port 8282 &
-fi
-
--- ubuntu/src/home/Documents/installation/tar_profiles_firefox.sh
+++
@@ -1,4 +0,0 @@
-readonly HOME_INSTALL=/home/headless/Documents/installation
-
-cd /home/headless/.mozilla/firefox
-tar cfa $HOME_INSTALL/profiles_firefox.tar.xz *
