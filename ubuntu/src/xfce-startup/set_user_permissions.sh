c2f3a76c5baee6d776b25bcbc56d8b1c78d2f8c6
Bertrand DELAITRE <bertrand@delaitre.fr>
2024-07-30 06:18:06 +0200

    remove parameters for set_user_permissions.sh

--- ubuntu/src/xfce-startup/set_user_permissions.sh
+++ ubuntu/src/xfce-startup/set_user_permissions.sh
@@ -3,22 +3,23 @@
 #set -u     # ne pas utiliser
 
 ### Corriger les permissions des fichiers
-for i in "$@"; do
-
-
+changePermissions() {
     ### définir les permissions des répertoires
     ### de manière récursive, mais en ignorant les répertoires point dans $HOME
-    find "$i" -type d -not -path "${HOME}/.*" -exec chmod $verbose 755 {} +
+    find "$1" -type d -not -path "${HOME}/.*" -exec chmod $verbose 755 {} +
 
     ### définir les permissions des fichiers
     ### de manière récursive, mais en ignorant les fichiers point et les répertoires point dans $HOME
-    find "$i" -type f -not -path "${HOME}/.*" -exec chmod $verbose 644 {} +
+    find "$1" -type f -not -path "${HOME}/.*" -exec chmod $verbose 644 {} +
 
     ### permissions spécifiques des fichiers
     ### de manière récursive, mais en ignorant les répertoires point dans $HOME
-    find "$i" -type f -not -path "${HOME}/.*" -name '*.sh' -exec chmod $verbose 744 {} +
-    find "$i" -type f -not -path "${HOME}/.*" -name '*.desktop' -exec chmod $verbose 744 {} +
-done
+    find "$1" -type f -not -path "${HOME}/.*" -name '*.sh' -exec chmod $verbose 744 {} +
+    find "$1" -type f -not -path "${HOME}/.*" -name '*.desktop' -exec chmod $verbose 744 {} +
+}
+
+changePermissions "${HOME}"
+changePermissions "${STARTUPDIR}"
 
 ### le script de démarrage est spécial
 chmod 755 "${STARTUPDIR}/startup.sh"
